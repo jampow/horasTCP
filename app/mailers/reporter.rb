b47f48377cf5d67cce262b70e0a8fe6ae4ad03a4
Gianpaulo M. Soares <jam_pow@hotmail.com>
2015-11-15 12:36:26 -0200

    Carregando helper para ser utilizado no envio de e-mail

--- app/mailers/reporter.rb
+++ app/mailers/reporter.rb
@@ -1,6 +1,8 @@
 class Reporter < ApplicationMailer
 	default from: "gianpaulosoares@gmail.com"
 
+	add_template_helper(ApplicationHelper)
+
 	def visit(visit)
 		@visit = visit
 		mail(to: "jam_pow@hotmail.com", subject: "Manutenção " + visit.day.strftime("%d/%m/%Y") )
