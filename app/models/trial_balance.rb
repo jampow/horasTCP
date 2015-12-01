4f74e434b5d93b0efc9923772291be1324ef1d63
Gianpaulo M. Soares <jam_pow@hotmail.com>
2015-12-01 01:08:30 -0200

    Definindo o mês anterior automaticamente na criação do elemento

--- app/models/trial_balance.rb
+++ app/models/trial_balance.rb
@@ -1,2 +1,19 @@
 class TrialBalance < ActiveRecord::Base
+
+	after_initialize :set_last_month
+
+private
+
+	def set_last_month
+	    today = Date.today
+
+	    if today.month - 1 == 0
+	      self.year = today.year - 1
+	      self.month = 12
+	    else
+	      self.year = today.year
+	      self.month = today.month - 1
+	    end
+	end
+
 end
