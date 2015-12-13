9b96dced193cce4acdba234c2f004dc8e622e881
Gianpaulo M. Soares <jam_pow@hotmail.com>
2015-12-13 00:06:24 -0200

    Corrigindo calculos com angular

--- app/assets/javascripts/trial_balances.js
+++ app/assets/javascripts/trial_balances.js
@@ -1,11 +1,9 @@
 angular.module('trialBalance', [])
-.controller('TrialBalanceController', [$scope, function($scope) {
+.controller('TrialBalanceController', function() {
   this.valueHour = 90;
   this.totalValue = 0;
 
-
-  $scope.calcTotal = function () {
-    this.totalValue = this.totalHour * this.valueHour;
-    console.log(this.totalValue);
+  this.calcTotal = function calcTotal() {
+    this.totalValue = this.totalHours * this.valueHour;
   };
-}]);
+});
